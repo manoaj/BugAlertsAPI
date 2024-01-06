446ed26aa7cd971815c4d5c5d7ff8d62de8b86d5
manoaj <110446508+manoaj@users.noreply.github.com>
2024-01-06 09:18:21 +0530

    Add files via upload

---
+++ code/Cred.py
@@ -0,0 +1,12 @@
+hostname = "tacteam2"
+username = ""   #add your SSO username
+password = "*"   #add your SSO password
+port = 22
+
+base_URL = ""
+
+DB_USER = "postgres"
+DB_PASSWORD = "password"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "bugAlerts"
